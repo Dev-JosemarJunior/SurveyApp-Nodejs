387ebb9320b7cfe6520841559518db654169ddcd
Dev-JosemarJunior <dev.josemarjunior@gmail.com>
2020-12-28 02:14:54 -0300

    Allows passport to use cookies and logout the user

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express');
+// Mongoose is make more easy to work with MongoDB with javascript.
 const mongoose = require('mongoose');
+// CookieSession is for enable a possibility to use cookie to identify an user.
+const cookieSession = require('cookie-session');
+// We need to call passport again to 'him' understand we need to work with cookieSession together.
+const passport = require('passport');
+// These keys require is for keep some information proteced. This file is specified in .gitignore
 const keys = require('./config/keys');
-//This 'user require' is to use mongoose and mongoDB as our database. All setup and format of collections will be running in this file.
+// This 'user require' is to use mongoose and mongoDB as our database. All setup and format of collections will be running in this file.
 require('./models/User');
-//This 'require' bellow just executes the code inside this file. Is not necessary to use a variable to handle it because we will use intere file, like a module.
+// This 'require' bellow just executes the code inside this file. Is not necessary to use a variable to handle it because we will use intere file, like a module.
 require('./services/passport');
 
-
+// Function to connect mongoose with our database on cloud. Important to specify that a token necessary is protected in keys file.
 mongoose.connect(keys.mongoURI);
 
+// This is a kind of heart of our application. Express is responsible to comunicate with user by HTTP protocols.
 const app = express();
 
-//This 'require' returns a function inside this file. Just because have only one function, immediately calling this argument (app) means the function will runs with this argument.
+// Here we are instructing Express to use cookie. Maxage specify how much time the cookie will be available. Keys use some keys (in keys file) to generate a encripted cookie.
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+// Thoose functions are to initialize and authenticates requests telling passport to use cookies to this process. While initialize() begins the process, session() is part of deserializeUser process.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// This 'require' returns a function inside this file. Just because have only one function, immediately calling this argument (app) means the function will runs with this argument.
 require('./routes/authRoutes')(app);
 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,25 @@
 const passport = require('passport');
 
-//This handles executes passport logic sending a user to Google page, when will be required from user a authorization to use his Google account information.
+// This handles executes passport logic sending a user to Google page, when will be required from user a authorization to use his Google account information.
 module.exports = app =>{
-    //First handle send a user to Google page.
+    // First handle send a user to Google page.
     app.get(
         '/auth/google', passport.authenticate('google', {
             scope: ['profile', 'email']
-        }
-    ))
+        })
+    );
     
-    //Second one returns his information to our server.
+    // Second one returns his information to our server.
     app.get('/auth/google/callback', passport.authenticate('google'));
+    
+    // This rout is responsible to logout user. When is called the user is no more login. Is possible checkout acessing '/api/current_user" and if is blank means that the app shut you down.
+    app.get('/api/logout', (req, res) =>{
+        req.logout();
+        res.send(req.user);
+    })
+
+    // Just a test function to we can see some user information from database. If we have information is because the user is logged in. If not he just logged out.
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    })
 };
