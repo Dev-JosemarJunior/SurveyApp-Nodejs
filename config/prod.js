0b9cd1f92f54bedaa55c79d6063b9cb26b970589
Dev-JosemarJunior <dev.josemarjunior@gmail.com>
2020-12-31 00:37:37 -0300

    New config keys configurations to able Heroku to use env variables and hide our dev-keys.

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
-// Production keys here. Thoose variebles are configurated on Heroku.
+// Production keys here. Those variables are configured on Heroku.
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENTE_ID,
     googleClientSecret: process.env.GOOGLE_CLIENTE_SECRET,
-    mongoURI: process.env.GOOGLE_MONGO_URI,
-    cookieKey: process.env.GOOGLE_COOKIE.KEY
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 }
